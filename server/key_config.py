d45f34fe0a38ca3a805024a54b3fc27b8eb9d0e0
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2024-01-31 12:24:39 +0530

    hid the access key

--- server/key_config.py
+++ server/key_config.py
@@ -1,2 +1,2 @@
-Access_key_ID='AKIA3FRXXJQCIDZXEEFB'
-Secret_access_key='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'
+# Access_key_ID='AKIA3FRXXJQCIDZXEEFB'
+# Secret_access_key='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'
